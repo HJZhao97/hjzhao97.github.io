a3bac0e3188efda0c6231b11bbbdaaad81baad72
HJZhao97 <1695102532@qq.com>
2021-04-13 12:47:05 +0800

    add content of dist

---
+++ static/js/4.2ed341797cadcea7325e.js
@@ -0,0 +1,2 @@
+webpackJsonp([4],{XRiw:function(e,t,s){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"blogMessage"},[this._v("blog留言")])},staticRenderFns:[]};var a=s("VU/8")({name:"BlogMessage"},n,!1,function(e){s("hxv2")},"data-v-34c39199",null);t.default=a.exports},hxv2:function(e,t){}});
+//# sourceMappingURL=4.2ed341797cadcea7325e.js.map
