bb1e2d726b2454ba8d710d3c91b2bbd1acc6b962
HJZhao97 <1695102532@qq.com>
2021-04-14 19:13:14 +0800

    头部组件增加轮播图效果，轮播图可视为独立的vue组件

---
+++ static/js/2.90010a76aa7dbb8d4525.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{"+p9t":function(t,e){},"2kkt":function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var s={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"about-me"},[e("h2",[this._v("关于我")])])}]};var i=n("VU/8")({name:"AboutMe"},s,!1,function(t){n("+p9t")},"data-v-637bfe7e",null);e.default=i.exports}});
+//# sourceMappingURL=2.90010a76aa7dbb8d4525.js.map
